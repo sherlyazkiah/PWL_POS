77aea10b54ababdce1a0fb79296e522a9ce1b7cc
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-03-02 20:39:56 +0700

    query builder

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('level', [LevelController::class, 'index']);
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
