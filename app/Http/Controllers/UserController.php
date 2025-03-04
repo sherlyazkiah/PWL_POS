0aeaaf655afec257609e525b68b874b0dfabd783
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-03-04 12:01:34 +0700

    not found exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,9 +10,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user = UserModel::findOr(10, ['username', 'nama'], function() {
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
