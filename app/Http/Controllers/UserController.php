c8fa589131d276b4bf91c4d1dff4d09406476ee1
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-03-04 11:12:12 +0700

    $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,19 +10,13 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        // pastikan m_level memiliki level_id = 4, karena level_id adalah foreign key
-        // di tabel m_user, maka kalau tidak ada akan error
-        DB::table('m_level')->insertOrIgnore([
-            'level_id' => 4,
-            'level_kode' => 'CUST',
-            'level_nama' => 'Customer'
-        ]);
-
-        // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama'
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data);
 
         //coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
