bcae99e3a00aa6a165a7898a12aeb45ad86365c7
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-03-10 15:40:05 +0700

    relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user = UserModel::all();
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
 
