bc1e508788f97fcd7c5944e08a4368e5ed5be670
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-04-08 01:31:13 +0700

    multi-level authorization  implementation

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -44,4 +44,12 @@ class UserModel extends Authenticatable
     {
         return $this->level->level_kode == $role;
     }
+
+    /**
+     * Mendapatkan kode role
+     */
+    public function getRole()
+    {
+        return $this->level->level_kode;
+    }
 }
