db544ebcdc14f406e21eab2f1fca646d600f4f98
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-05-04 22:23:29 +0700

    api week 9

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,9 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
+    }
+
     use HasFactory;
 
     protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
